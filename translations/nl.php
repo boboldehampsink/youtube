f7dd10c2ad64e46154daff57aa3a778e2cc8b9d0
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-04-04 14:17:13 +0200

    Added instruction text

--- translations/nl.php
+++ translations/nl.php
@@ -4,4 +4,5 @@ return array(
     'All video hashes' => 'Alle video hashes',
     'Delete video hash(es)' => 'Video hash(es) verwijderen',
     'Hash(es) deleted' => 'Hash(es) verwijderd',
+    'This is a list of video\'s you\'ve already uploaded to YouTube once<br>If you deleted a video from YouTube and want to upload it again, you will have to delete it here first.' => 'Dit is een lijst van video\'s die je al eens naar YouTube hebt geupload.<br>Als je een video van YouTube hebt verwijderd, en opnieuw wilt uploaden, dien je hem eerst hier te verwijderen.',
 );
