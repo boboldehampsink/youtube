1c07221c602b4ba5d73984512b493b06ff470ed8
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-04-05 16:06:54 +0200

    Better translations

--- translations/nl.php
+++ translations/nl.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'All video hashes' => 'Alle video hashes',
-    'Delete video hash(es)' => 'Video hash(es) verwijderen',
-    'Hash(es) deleted.' => 'Hash(es) verwijderd',
+    'All video hashes' => 'Alle video referenties',
+    'Delete video hash(es)' => 'Video referentie(s) verwijderen',
+    'Hash(es) deleted.' => 'Referentie(s) verwijderd',
     'This is a list of video\'s you\'ve already uploaded to YouTube once<br>If you deleted a video from YouTube and want to upload it again, you will have to delete it here first.' => 'Dit is een lijst van video\'s die je al eens naar YouTube hebt geupload.<br>Als je een video van YouTube hebt verwijderd, en opnieuw wilt uploaden, dien je hem eerst hier te verwijderen.',
 );
